b6127fe629a19610ac3839f2cc089f26958a071e
Johannes Ringmark <johannes.ringmark@gmail.com>
2025-09-02 19:20:05 +0200

    Initial Commit

---
+++ Quizzie/QuizzieApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzieApp.swift
+//  Quizzie
+//
+//  Created by johannes Ringmark on 2025-08-31.
+//
+
+import SwiftUI
+
+@main
+struct QuizzieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizzieTests/QuizzieTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzieTests.swift
+//  QuizzieTests
+//
+//  Created by johannes Ringmark on 2025-08-31.
+//
+
+import Testing
+@testable import Quizzie
+
+struct QuizzieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
